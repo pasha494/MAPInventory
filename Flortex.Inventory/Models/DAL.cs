fb61a124f375135d65b51d33225e63e9d1419d4f
Pasha <Pasha494@gmail.com>
2017-10-07 15:36:08 +0530

    Addeed Master Controller

--- Flortex.Inventory/Models/DAL.cs
+++ Flortex.Inventory/Models/DAL.cs
@@ -65,7 +65,7 @@ namespace MAP.Inventory.Models
                 oCon.Close();
             }
             catch
-            {
+            { 
             }
             return iResult;
         }
